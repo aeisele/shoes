bb5bdfaba54c9b74934b38fd1f8b56c726f3e960
Andreas Eisele <ae@andreaseisele.com>
2020-08-31 16:13:19 +0200

    count distinct

---
+++ src/main/java/distinct/Solution.java
@@ -0,0 +1,25 @@
+package distinct;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public int solution(int[] A) {
+
+        if (A.length == 0) {
+            return 0;
+        }
+
+        Arrays.sort(A);
+
+        int count = 1;
+        for (int i = 1; i < A.length; i++) {
+            if (A[i] != A[i - 1]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+}
